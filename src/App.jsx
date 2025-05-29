d551481f1c9848a1a87b3c2512ef82c2dc0cb096
Shipul <shipul2910@gmail.com>
2025-05-29 20:59:19 +0530

    react-router-setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router";
+
 function App() {
   return (
     <>
-      <div className="py-8 bg-amber-200 flex">
-        <p className="text-3xl font-bold">BLOGPOD</p>
+      <div className="py-8 bg-slate-100">
+        <h1 className="text-3xl font-bold">BLOGPOD</h1>
+        <Outlet />
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+//Components
+import Home from "./pages/Home.jsx";
+import AllBlogs from "./pages/AllBlogs.jsx";
+import AddBlog from "./pages/AddBlog.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Profile from "./pages/Profile.jsx";
+import Logout from "./pages/Logout.jsx";
+import SingleBlog from "./pages/SingleBlog.jsx";
+import Contact from "./pages/Contact.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      { index: true, Component: Home },
+      { path: "all-blogs", Component: AllBlogs },
+      { path: "add-blog", Component: AddBlog },
+      { path: "auth/login", Component: Login },
+      { path: "auth/signup", Component: Signup },
+      { path: "profile", Component: Profile },
+      { path: "logout", Component: Logout },
+      { path: "blog/:slug", Component: SingleBlog },
+      { path: "contact", Component: Contact },
+      { path: "*", Component: NotFound },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
