5db06382a8723198f88494cf905434a3497d0b34
Shipul <shipul2910@gmail.com>
2025-05-29 20:04:59 +0530

    Initial project code

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="py-8 bg-amber-200 flex">
+        <p className="text-3xl font-bold">BLOGPOD</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
