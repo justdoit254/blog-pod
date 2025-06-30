2d2d77de58a529b06c66fd3e6b1f5c8bc4ee013d
Shipul <shipul2910@gmail.com>
2025-06-30 21:09:50 +0530

    contact and subscriber collections

--- src/config.js
+++ src/config.js
@@ -3,6 +3,8 @@ const config = {
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteContactCollectionId: String(import.meta.env.VITE_APPWRITE_CONTACT_COLLECTION_ID),
+    appwriteSubscribeCollectionId: String(import.meta.env.VITE_APPWRITE_SUBSCRIBE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
